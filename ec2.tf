bc8c3826cedcc56f06760974fd408eaa4e2a8def
safamarzouki-code <126520713+safamarzouki-code@users.noreply.github.com>
2023-03-17 00:50:51 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
-provider "aws"{
-region = "us-east-1"
+provider "aws" {
+   region = "us-east-1"
 }
-
 resource "aws_instance" "myec2" {
     ami = "ami-012cc038cc685a0d7"
     instance_type = "t2.micro"
